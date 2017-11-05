3d8571f1fee0ebe149da365059daef9419ac866f
Yannick Martel <ymartel@gmail.com>
2017-11-05 10:48:17 +0100

    playing -> optimized catboost on internet test

--- quality_benchmarks/catboost_experiment.py
+++ quality_benchmarks/catboost_experiment.py
@@ -6,25 +6,27 @@ import os, numpy as np
 
 class CABExperiment(Experiment):
 
-    def __init__(self, learning_task, n_estimators=5000, max_hyperopt_evals=50, 
-                 counters_sort_col=None, holdout_size=0, 
+    def __init__(self, learning_task, n_estimators=5000, max_hyperopt_evals=50,
+                 counters_sort_col=None, holdout_size=0,
                  train_path=None, test_path=None, cd_path=None, output_folder_path='./'):
         assert holdout_size == 0, 'For Catboost holdout_size must be equal to 0'
-        Experiment.__init__(self, learning_task, 'cab', n_estimators, max_hyperopt_evals, 
-                            False, None, holdout_size, 
+        Experiment.__init__(self, learning_task, 'cab', n_estimators, max_hyperopt_evals,
+                            False, None, holdout_size,
                             train_path, test_path, cd_path, output_folder_path)
 
         self.space = {
             'depth': hp.choice('depth', [6]),
             'ctr_border_count': hp.choice('ctr_border_count', [16]),
             'border_count': hp.choice('border_count', [128]),
-            'ctr_description': hp.choice('ctr_description', [['Borders','CounterMax']]),
+            'ctr_description': hp.choice('ctr_description', [['Borders','Counter']]),
             'learning_rate': hp.loguniform('learning_rate', -5, 0),
             'random_strength': hp.choice('random_strength', [1, 20]),
             'one_hot_max_size': hp.choice('one_hot_max_size', [0, 25]),
             'l2_leaf_reg': hp.loguniform('l2_leaf_reg', 0, np.log(10)),
             'bagging_temperature': hp.uniform('bagging_temperature', 0, 1),
             'used_ram_limit': hp.choice('used_ram_limit', [100000000000]),
+#            'od_type': hp.choice("od_type", ["Iter"]),
+#            'od_wait': hp.choice("od_wait", [40]),
         }
 
         if learning_task == 'classification':
@@ -42,22 +44,24 @@ class CABExperiment(Experiment):
             'l2_leaf_reg': 3,
             'leaf_estimation_method': 'Newton',
             'gradient_iterations': 10,
-            'ctr_description': ['Borders','CounterMax'],
+            'ctr_description': ['Borders','Counter'],
             'used_ram_limit': 100000000000,
+#            'od_type': "Iter",
+#            'od_wait': 40,
         }
         self.default_params = self.preprocess_params(self.default_params)
         self.title = 'CatBoost'
 
-    
+
     def convert_to_dataset(self, data, label, cat_cols):
         return Pool(data, label, cat_features=cat_cols)
 
 
     def preprocess_params(self, params):
         if self.learning_task == 'classification':
-            params.update({'loss_function': 'Logloss', 'verbose': False, 'thread_count': 16, 'random_seed': 0})
+            params.update({'loss_function': 'Logloss', 'verbose': False, 'thread_count': 8, 'random_seed': 0})
         elif self.learning_task == 'regression':
-            params.update({'loss_function': 'RMSE', 'verbose': False, 'thread_count': 16, 'random_seed': 0})
+            params.update({'loss_function': 'RMSE', 'verbose': False, 'thread_count': 8, 'random_seed': 0})
         return params
 
 
@@ -67,8 +71,10 @@ class CABExperiment(Experiment):
         bst = CatBoost(params)
         bst.fit(dtrain, eval_set=dtest)
         with open("test_error.tsv", "r") as f:
-            results = np.array(map(lambda x: float(x.strip().split()[-1]), f.readlines()[1:]))
-        
+#            results = np.array([
+#                np.array(map(lambda x: float(x.strip().split()[-1]), f.readlines()[1:])).min()] * 3) ##YY
+            results = np.array(map(lambda x: float(x.strip().split()[-1]), f.readlines()[1:]) ##YY
+
         return bst, results
 
     def predict(self, bst, dtest, X_test):
@@ -76,4 +82,3 @@ class CABExperiment(Experiment):
         if self.learning_task == 'classification':
             preds = np.power(1 + np.exp(-preds), -1)
         return preds
-
